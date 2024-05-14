0e861e978c6ae1e85b39c6f40a28662c704dd3af
Gustavo Felippe Barbosa <gustavofelippeb5@gmail.com>
2024-05-14 14:25:55 -0300

    v 1.1
    Componentes Card, Form e Title criados e a TelaPrincipal também

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import './Title.css';
+
+const Title = () => {
+  return (
+    <div className='title'>
+      <h3>Encontre seu</h3>
+      <h2>Agente de Sucesso</h2>
+    </div>
+  );
+};
+
+export default Title;
