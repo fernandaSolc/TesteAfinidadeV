ab3f3c654bca31c956121f3a15f026a90528267f
Gustavo Felippe Barbosa <gustavofelippeb5@gmail.com>
2024-05-15 11:36:25 -0300

    v 1.2
    Componentes foram reorganizados

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import LogoPD from '../../img/logoPD.svg';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={LogoPD} alt='Início do Quiz' />
+    </div>
+  );
+};
+
+export default Logo;
