ce9c0550cba9ad6fe6e545c847cbd8f70777f01e
fernandasolc <fernandaoliv.8272@gmail.com>
2024-05-20 15:01:13 -0300

    pipipipopopo

--- src/components/CardRespostas/CardRespostas.jsx
+++ src/components/CardRespostas/CardRespostas.jsx
@@ -2,27 +2,7 @@ import './CardRespostas.css';
 
 const CardRespostas = () => {
   return (
-    <div>
-      <form>
-        <div>
-          <input type='radio' id='html' name='fav_language' value='HTML' />
-          <label htmlFor='html'>HTML</label>
-        </div>
-        <div>
-          <input type='radio' id='css' name='fav_language' value='CSS' />
-          <label htmlFor='css'>CSS</label>
-        </div>
-        <div>
-          <input
-            type='radio'
-            id='javascript'
-            name='fav_language'
-            value='JavaScript'
-          />
-          <label htmlFor='javascript'>JavaScript</label>
-        </div>
-      </form>
-    </div>
+   
   );
 };
 
