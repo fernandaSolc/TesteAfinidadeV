3d29b1b253f06e476aa8f249ab681339763f2ef0
GustavoFlpp <gustavofelippeb5@gmail.com>
2024-06-04 09:53:54 -0300

    tomara que suba

--- src/pages/TelaPrincipal/TelaPrincipal.jsx
+++ src/pages/TelaPrincipal/TelaPrincipal.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import FormTelaPrincipal from '../../components/FormTelaPrincipal/FormTelaPrincipal';
+import Card from '../../components/Card/Card';
+import Logo from '../../components/Logo/Logo';
+import Title from '../../components/Title/Title';
 import './TelaPrincipal.css';
 
 const TelaPrincipal = () => {
@@ -8,12 +11,16 @@ const TelaPrincipal = () => {
 
   return (
     <div className='telaPrincipal'>
-      <FormTelaPrincipal
-        pdx={pdx}
-        setPdx={setPdx}
-        turno={turno}
-        setTurno={setTurno}
-      />
+      <Title />
+      <Card>
+        <Logo />
+        <FormTelaPrincipal
+          pdx={pdx}
+          setPdx={setPdx}
+          turno={turno}
+          setTurno={setTurno}
+        />
+      </Card>
     </div>
   );
 };
