a18e2bad04608abd14be2465fb5e7df3be0f015c
larissafr1 <larissafelipereis7@gmail.com>
2024-06-10 09:33:50 -0300

    formulário e lógica de match

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TelaPerguntas from './pages/TelaPerguntas/TelaPerguntas';
-import TelaPrincipal from './pages/TelaPrincipal/TelaPrincipal';
-import CardTelaVerificarAluno from './components/CardTelaVerificarAluno/CardTelaVerificarAluno'
+import "./App.css";
+import { useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TelaPrincipal from "./pages/TelaPrincipal/TelaPrincipal";
+import CardTelaVerificarAluno from "./components/CardTelaVerificarAluno/CardTelaVerificarAluno";
+import FormTelaPerguntas from "./components/FormTelaPerguntas/FormTelaPerguntas";
+import TelaEscolhaAgentes from "./pages/TelaEscolhaAgentes/TelaEscolhaAgentes"
 
+const FormWrapper = () => {
+  const location = useLocation();
+  const turno = location.state?.turno;
+
+  return <FormTelaPerguntas turno={turno} />;
+};
+
+const ConfirmacaoWrapper = () => {
+  const location = useLocation();
+  const bestMonitor = location.state?.bestMonitor;
+
+  return <TelaEscolhaAgentes bestMonitor={bestMonitor} />;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<TelaPrincipal />} />
-        <Route path='/confirmacao' element={<CardTelaVerificarAluno />} />
-        <Route path='/form' element={<TelaPerguntas />} />
+        <Route path="/" element={<TelaPrincipal />} />
+        <Route path="/confirmacao" element={<CardTelaVerificarAluno />} />
+        <Route path="/form" element={<FormWrapper />} />
+        <Route path="/agente" element={<ConfirmacaoWrapper />} />
       </Routes>
     </Router>
   );
